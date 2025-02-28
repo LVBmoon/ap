2c4f7961ba879b0bb875b9aad31cbd83ea6912b3
LVB <mobinaseif84@gmail.com>
2025-02-28 15:39:11 +0330

    solution for Exercise5.18 version 1: sort strings lexicographically

---
+++ Exercise5_18_ver1.java
@@ -0,0 +1,37 @@
+/*Write a program that reads in three strings and sorts them lexicographically.
+Enter three strings: Charlie Able Baker
+Able
+Baker
+Charlie*/
+
+import java.util.Scanner;
+
+public class Exercise5_18_ver1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three strings: ");
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String str3 = sc.next();
+        String temp;
+        if (str1.compareTo(str2) > 0) {
+            temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+        if (str2.compareTo(str3) > 0) {
+            temp = str2;
+            str2 = str3;
+            str3 = temp;
+        }
+        if (str1.compareTo(str3) > 0) {
+            temp = str1;
+            str1 = str3;
+            str3 = temp;
+        }
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
