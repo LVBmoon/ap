f631de8fe0e9b4180a3cfb0f27ce19c75b40ea4d
LVB <mobinaseif84@gmail.com>
2025-03-02 16:28:59 +0330

    1403/12/12

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
     public class EvenOdd {
         public static void main(String[] args) {
-            System.out.println("Please enter a number: ");
             Scanner sc = new Scanner(System.in);
-
+            System.out.println("Please enter a number: ");
             int number = sc.nextInt();
             printEvenOdd(number);
             printEvenNumbersOfArange(number);
