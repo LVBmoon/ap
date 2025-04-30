4e7e95208279c3dd4417138eafdddeecc097b011
LVB <mobinaseif84@gmail.com>
2025-04-30 21:23:15 +0330

    EX4_E4_14

---
+++ ap/exercises/exe4/Main_EX4_E4_14.java
@@ -0,0 +1,13 @@
+package ap.exercises.exe4;
+
+public class Main_EX4_E4_14 {
+    public static void main(String[] args) {
+        String comb = "+--+--+--+";
+        String bottomLine = "|  |  |  |";
+        System.out.println(comb);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(bottomLine);
+            System.out.println(comb);
+        }
+    }
+}
