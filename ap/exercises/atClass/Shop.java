bef41f46aa3815c8193e8836adea519f9eb7a91c
LVB <mobinaseif84@gmail.com>
2025-05-06 11:21:33 +0330

    Shop question (the complete one)

--- ap/exercises/atClass/Shop.java
+++ ap/exercises/atClass/Shop.java
@@ -2,52 +2,58 @@ package ap.exercises.atClass;
 
 import java.util.ArrayList;
 
+/*this is the complete code I wanted to write but I got out of time I
+changed the List because the two of the classes extends Iteam
+and then use for to sout in the terminal (each sout cll the toString I wrote)
+*/
 public class Shop {
     public static void main(String[] args) {
-        ArrayList<Book> bookList = new ArrayList<>();
-        ArrayList<Pen> penList = new ArrayList<>();
+        ArrayList<Iteam> itemList = new ArrayList<>();
 
-        bookList.add(new Book("Math", 100));
-        bookList.add(new Book("Science", 200));
-        penList.add(new Pen(200,"Black","bi"));
-        penList.add(new Pen(200,"Blue","bi"));
+        itemList.add(new Book("Math", 100));
+        itemList.add(new Book("Science", 200));
+        itemList.add(new Pen(200, "Black", "bi"));
+        itemList.add(new Pen(200, "Blue", "bi"));
 
+        for (Iteam item : itemList) {
+            System.out.println(item);
+        }
     }
 }
 
 class Iteam {
     protected int price;
-    public String toString(){
-        return "0";
-    }
 
+    public String toString() {
+        return "Item with price: " + price;
+    }
 }
 
 class Book extends Iteam {
     private String title;
-    private int bookPrice;
 
-    public Book(String title, int bookPrice){
+    public Book(String title, int price) {
         this.title = title;
-        this.bookPrice = bookPrice;
+        this.price = price;
     }
 
     @Override
     public String toString() {
-        return "title=" + title + ", bookPrice=" + bookPrice;
+        return "Book Title: " + title + ", Price: " + price;
     }
 }
 
 class Pen extends Iteam {
+    private String color, brand;
 
-    private String color,brand;
     public Pen(int price, String color, String brand) {
         this.price = price;
         this.color = color;
         this.brand = brand;
     }
+
     @Override
     public String toString() {
-        return "Price : " + price + " Color : " + color + " Brand : " + brand;
+        return "Pen - Price: " + price + ", Color: " + color + ", Brand: " + brand;
     }
 }
