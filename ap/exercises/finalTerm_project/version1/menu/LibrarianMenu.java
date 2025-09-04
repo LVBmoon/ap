bb312305af686d6593e16fc150cba1d10ba39f63
LVB <mobinaseif84@gmail.com>
2025-09-04 22:35:17 +0330

    FPR_3-1 Librarian login

--- ap/exercises/finalTerm_project/version1/menu/LibrarianMenu.java
+++ ap/exercises/finalTerm_project/version1/menu/LibrarianMenu.java
@@ -1,4 +1,50 @@
 package ap.exercises.finalTerm_project.version1.menu;
 
-public class LibrarianMenu {
+import src.selfTraining.version0.core.Library;
+import src.selfTraining.version0.data.InputProcessor;
+import src.selfTraining.version0.menu.Menu;
+import src.selfTraining.version0.model.Librarian;
+
+public class LibrarianMenu extends Menu {
+    private Librarian librarian;
+
+    public LibrarianMenu(Library library, InputProcessor inputProcessor) {
+        super(library, inputProcessor);
+        this.librarian = null;
+    }
+
+    @Override
+    public void showMenu() {
+        while (true) {
+            if (librarian == null) {
+                System.out.println("\n=== Librarian Menu ===");
+                System.out.println("   1. Login");
+                System.out.println("   2. Exit");
+                int choice = inputProcessor.getIntInput("Please enter choice: ");
+                switch (choice) {
+                    case 1:
+                        login();
+                        break;
+                    case 2:
+                        return;
+                    default:
+                        System.out.println("\nInvalid choice!");
+                }
+            }
+        }
+    }
+
+    private void login() {
+        String username = inputProcessor.getStringInput("Enter username: ");
+        String password = inputProcessor.getStringInput("Enter password: ");
+        for (Librarian l : library.getLibrarians()) {
+            if (l.getUsername().equals(username) && l.checkPassword(password)) {
+                librarian = l;
+                System.out.println("\nLogin successful.");
+                return;
+            }
+        }
+        System.out.println("\nInvalid username or password!");
+    }
 }
+
