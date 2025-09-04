fd67f8c3fb49d3b9af20cd37effbc59aaf0be70e
LVB <mobinaseif84@gmail.com>
2025-09-04 22:40:30 +0330

    FPR_3-2 Librarian change password function

--- ap/exercises/finalTerm_project/version1/menu/LibrarianMenu.java
+++ ap/exercises/finalTerm_project/version1/menu/LibrarianMenu.java
@@ -1,6 +1,7 @@
 package ap.exercises.finalTerm_project.version1.menu;
 
 import src.selfTraining.version0.core.Library;
+import src.selfTraining.version0.data.DataStorage;
 import src.selfTraining.version0.data.InputProcessor;
 import src.selfTraining.version0.menu.Menu;
 import src.selfTraining.version0.model.Librarian;
@@ -12,7 +13,6 @@ public class LibrarianMenu extends Menu {
         super(library, inputProcessor);
         this.librarian = null;
     }
-
     @Override
     public void showMenu() {
         while (true) {
@@ -30,7 +30,28 @@ public class LibrarianMenu extends Menu {
                     default:
                         System.out.println("\nInvalid choice!");
                 }
+            } else {
+                System.out.println("\n--- Librarian Menu ---\nWelcome " + librarian.getFirstName() + " " + librarian.getLastName() + "!");
+                System.out.println("   1. Edit Personal Info");
+                System.out.println("   2. Add New Book");
+                System.out.println("   3. Edit Book");
+                System.out.println("   4. Review Borrow Requests");
+                System.out.println("   5. Review Return Requests");
+                System.out.println("   6. View Processed Borrows");
+                System.out.println("   7. View Student Borrow History");
+                System.out.println("   8. Change Password");
+                System.out.println("   9. Toggle Student Active Status");
+                System.out.println("   10. Logout");
+                int choice = inputProcessor.getIntInput("Please enter choice: ");
+                switch (choice) {
+                    case 8:
+                        changePassword();
+
+                    default:
+                        System.out.println("\nInvalid choice!");
+                }
             }
+            new DataStorage().saveLibrary(library);
         }
     }
 
@@ -45,6 +66,21 @@ public class LibrarianMenu extends Menu {
             }
         }
         System.out.println("\nInvalid username or password!");
+    }
+
+    private void changePassword() {
+        String currentPass = inputProcessor.getStringInput("Enter current password: ");
+        if (!librarian.checkPassword(currentPass)) {
+            System.out.println("\nIncorrect current password!");
+            return;
+        }
+        String newPass = inputProcessor.getStringInput("Enter new password: ");
+        try {
+            librarian.setPassword(newPass);
+            System.out.println("\nPassword changed successfully!");
+        } catch (IllegalArgumentException e) {
+            System.out.println("\nPassword change failed: " + e.getMessage());
+        }
     }
 }
 
